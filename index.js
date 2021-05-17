1709ecfa0fc5adddf6e59e3e6bc4f8d80e176800
Marlon20-jar <marlon.jair@outlook.com>
2021-05-17 12:06:30 -0500

    added ternary operator, filter method and body parser

--- index.js
+++ index.js
@@ -3,18 +3,30 @@ const app = express();
 const { empleados } = require('./HR.json');
 
 app.get("/", (req, res, next)=> {
-    res.status(200);
-    res.send("Bievenido al API REST de Recursos Humanos");
+    return res.status(200).send("Bievenido al API REST de Recursos Humanos");
 });
 
 app.get('/empleados', (req, res, next) => {
-    res.status(200);
-    res.send(empleados);
+    return res.status(200).send(empleados);
 });
 
-app.get('/empleados/:id', (req, res, next) =>{
-    res.status(200);
-    res.send(empleados[req.params.id - 1]);
+app.get('/empleados/:id([0-9]{1,3})', (req, res, next) =>{
+    const id = req.params.id -1;
+    (id >= 0 && id <= 9) ?
+        res.status(200).send(empleados[req.params.id - 1]):
+        res.status(404).send("Empleado no encontrado");
+});
+
+app.get('/empleados/:name([A-Za-z]+)', (req, res, next) =>{
+    const name = req.params.name;
+
+    const emp = empleados.filter((e) => {
+        return (e.name.toUpperCase() == name.toUpperCase()) ? e : null;
+    }); 
+    
+    (emp.length > 0 ) ?  
+    res.status(200).send(emp) : 
+    res.status(404).send("Empleado no encontrado");
 });
 
 app.listen(process.env.PORT || 3000, () => {
