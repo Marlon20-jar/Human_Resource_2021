ddcb1b66e65f36eae6a0d3c6a68fb1e0edc3cd0f
Marlon20-jar <marlon.jair@outlook.com>
2021-05-16 19:55:03 -0500

    Management and access of express routes

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
+const { empleados } = require('./HR.json');
 
 app.get("/", (req, res, next)=> {
     res.status(200);
-    res.send("Bievenido");
+    res.send("Bievenido al API REST de Recursos Humanos");
 });
 
-app.listen(3000, () => {
+app.get('/empleados', (req, res, next) => {
+    res.status(200);
+    res.send(empleados);
+});
+
+app.get('/empleados/:id', (req, res, next) =>{
+    res.status(200);
+    res.send(empleados[req.params.id - 1]);
+});
+
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running...");
 });
