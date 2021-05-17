f17c89d11e5bc1c36ce6a9acf498bbf92c278ab6
Marlon20-jar <marlon.jair@outlook.com>
2021-05-17 12:56:23 -0500

    added POST and Body-Parser

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+const bodyParser = require( 'body-parser' );
 const express = require('express');
 const app = express();
 const { empleados } = require('./HR.json');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res, next)=> {
     return res.status(200).send("Bievenido al API REST de Recursos Humanos");
 });
+
+app.post("/empleados", (req, res, next) => {
+    return res.status(200).send(req.body);
+})
 
 app.get('/empleados', (req, res, next) => {
     return res.status(200).send(empleados);
@@ -12,21 +20,23 @@ app.get('/empleados', (req, res, next) => {
 
 app.get('/empleados/:id([0-9]{1,3})', (req, res, next) =>{
     const id = req.params.id -1;
-    (id >= 0 && id <= 9) ?
-        res.status(200).send(empleados[req.params.id - 1]):
-        res.status(404).send("Empleado no encontrado");
+    if (id >= 0 && id <= 9) { 
+        return res.status(200).send(empleados[req.params.id - 1]);
+    }
+    return res.status(404).send("Empleado no encontrado");
 });
 
 app.get('/empleados/:name([A-Za-z]+)', (req, res, next) =>{
     const name = req.params.name;
 
     const emp = empleados.filter((e) => {
-        return (e.name.toUpperCase() == name.toUpperCase()) ? e : null;
+        return (e.name.toUpperCase() == name.toUpperCase()) && e;
     }); 
-    
-    (emp.length > 0 ) ?  
-    res.status(200).send(emp) : 
-    res.status(404).send("Empleado no encontrado");
+
+    if (emp.length > 0 ) {   
+        return res.status(200).send(emp) ;
+    } 
+    return res.status(404).send("Empleado no encontrado");
 });
 
 app.listen(process.env.PORT || 3000, () => {
